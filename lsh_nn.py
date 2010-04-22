739fe1010d058824891ce7ef6faf01935f7e54dc
Hamilton <hamilton.ulmer@gmail.com>
2010-04-21 21:08:39 -0700

    Further reintegration of all the hashing functions with the actuall lsh_nn stuff.  Still not clear that this is the best way to organize these functions.

--- lsh_nn.py
+++ lsh_nn.py
@@ -69,27 +69,6 @@ def bin_signature_matrix(matrix):
 		signature_hash[v].append(i)
 	return signature_hash
 
-def minhash_test():
-	a = make_lsh_ensemble(make_minhash, number_of_functions=5, d=9)
-	li = [0,1,0,0,0,0,1,0,0]
-	li2 = [1,0,0,1,1,1,1,1,1]
-	print [mh(li) for mh in a]
-	print [mh(li2) for mh in a]
-
-def sparse_minhash_test():
-	smat = dok_matrix((1,100), dtype=np.int32)
-	smat[0,4] = 1
-	smat[0,6] = 1
-	smat[0,8] = 1
-	smat[0,85] = 1
-	smat[0,63] = 1
-	smat = csr_matrix(smat)
-	for i in range(10):
-		a = make_lsh_ensemble(make_sparse_minhash, number_of_functions=5, d=100)
-		sig = write_signature(smat[0,:], a)
-		print sig
-
-
 def lsh_nn(vector, signature_hash, original_data, distance, hash_ensemble):
 	vec_sig = write_signature(vector, hash_ensemble)
 	
