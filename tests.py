170e9b685a42c03b374e7116a38d9db90587e802
Hamilton <hamilton.ulmer@gmail.com>
2010-04-21 21:07:39 -0700

    Began a few tests for the functions in lsh_nn.py.  I will eventually switch to the unittest framework that comes bundled with Python, as soon as I devote a bit of time to testing the random parts.  I suppose setting the seed will ensure the exact same results every single time.

--- tests.py
+++ tests.py
@@ -6,8 +6,34 @@ Currently have minhash and coordinate hash implemented.
 from random import randint, shuffle
 from scipy.sparse import csr_matrix, dok_matrix
 import numpy as np
+from lsh_nn import make_minhash, make_sparse_minhash, make_coordinate_hash,
+                   make_lsh_ensemble, write_signature, write_signature_matrix,
+                   bin_signature_matrix
 
 
+#
+def minhash_test():
+	a = make_lsh_ensemble(make_minhash, number_of_functions=5, d=9)
+	li = [0,1,0,0,0,0,1,0,0]
+	li2 = [1,0,0,1,1,1,1,1,1]
+	print [mh(li) for mh in a]
+	print [mh(li2) for mh in a]
+
+def sparse_minhash_test():
+	smat = dok_matrix((1,100), dtype=np.int32)
+	smat[0,4] = 1
+	smat[0,6] = 1
+	smat[0,8] = 1
+	smat[0,85] = 1
+	smat[0,63] = 1
+	smat = csr_matrix(smat)
+	for i in range(10):
+		a = make_lsh_ensemble(make_sparse_minhash, number_of_functions=5, d=100)
+		sig = write_signature(smat[0,:], a)
+		print sig
+
+def sparse_matrix_write_signature():
+	pass
 
 if __name__ == "__main__":
 	#minhash_test()
