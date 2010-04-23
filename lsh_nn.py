d925d12f453222ff4745221078676bc52c5f4844
Hamilton <hamilton.ulmer@gmail.com>
2010-04-23 11:01:00 -0700

    (with great regret) tying together the basic lsh algorithm in a - gasp - LSH class.  Still pretty functional stuff, I guess.  Such is life.

--- lsh_nn.py
+++ lsh_nn.py
@@ -37,44 +37,50 @@ def make_coordinate_hash(d):
 	"""
 	n = randint(0,d-1)
 	def coordinate_hash(v):
+		"""Reduces a vector v to coordinate %s""" % n
 		return v[n]
+	coordinate_hash.__dict__['coord'] = n
 	return coordinate_hash
-
-def make_lsh_ensemble(hash_creator, number_of_functions=5, d=None):
-	"""Creates an ensemble of hash functions using 
-	a hash_creator.  You must specify the dimension 
-	of the data you're expecting, d, as well as the number_of_functions."""
-	return [hash_creator(d) for i in range(number_of_functions)]
 
 ##############################################################################
 ################### Functions For Building Signatures ########################
 ##############################################################################
 
-def write_signature(vector, hash_ensemble):
-	"""Turns a raw numeric vector into a signature, which is
-	simply a vector of hash functions with the original vector
-	as the argument."""
-	#print [ens(np.array(vector)[0]) for ens in hash_ensemble]
-	return np.array([ens(vector) for ens in hash_ensemble])
+class LSH_tools(object):
+	def __init__(self):
+		pass
+	
+	def make_lsh_ensemble(hash_creator, functions=5, d=None):
+		"""Creates an ensemble of hash functions using 
+		a hash_creator.  You must specify the dimension 
+		of the data you're expecting, d, as well as the number_of_functions."""
+		return [hash_creator(d) for i in range(functions)]
 
-def write_matrix_signature(matrix, hash_ensemble):
-	return np.matrix([write_signature(v, hash_ensemble) for v in matrix])
+	def write_signature(vector, hash_ensemble):
+		"""Turns a raw numeric vector into a signature, which is
+		simply a vector of hash functions with the original vector
+		as the argument."""
+		#print [ens(np.array(vector)[0]) for ens in hash_ensemble]
+		return np.array([ens(vector) for ens in hash_ensemble])
 
-def bin_signature_matrix(matrix):
-	signature_hash = {}
-	for i, v in enumerate(matrix):
-		v = tuple(np.array(v)[0])
-		if v not in signature_hash:
-			signature_hash[v] = []
-		signature_hash[v].append(i)
-	return signature_hash
+	def write_matrix_signature(matrix, hash_ensemble):
+		return np.matrix([write_signature(v, hash_ensemble) for v in matrix])
+
+	def bin_signature_matrix(matrix):
+		signature_hash = {}
+		for i, v in enumerate(matrix):
+			v = tuple(np.array(v)[0])
+			if v not in signature_hash:
+				signature_hash[v] = []
+			signature_hash[v].append(i)
+		return signature_hash 
 
 def lsh_nn(vector, signature_hash, original_data, distance, hash_ensemble):
 	vec_sig = write_signature(vector, hash_ensemble)
 	
 	candidates = signature_hash.get(vec_sig, None)
 	if not candidates:
-		raise Error, "Sadness.  There are no points in this hash. %s." % vec_sig
+		raise Error, "There are no points in this hash. %s." % vec_sig
 	winning_distance, winning_prototype = -1, -1
 	for c in candidates:
 		d = distance(vector, original_data[c,:])
@@ -82,6 +88,34 @@ def lsh_nn(vector, signature_hash, original_data, distance, hash_ensemble):
 			winning_distance = d
 			winning_prototype = c
 	return c
+
+class LSH(LSH_tools):
+	def __init__(self, hash_fcn = this_hash_fcn):
+		self.hash_function = this_hash_fcn
+		LSH_tools.__init__(self)
+		self.last_ensemble = None
+		self.last_indices = None
+		self.last_bins = None
+		self.last_data_id = None
+	
+	def bin_data(self, data, bands = 20, per_band = 5, flush = False):
+		"""Takes data and bins them using the LSH algorithm.
+		This is akin to the preprocessing step in LSH papers.
+		"""
+		sigsize = bands * per_bands
+		n, p = data.shape
+		# create the hash ensemble.
+		self.last_ensemble = self.make_lsh_ensemble(self.hash_function, 
+		                                           functions = sigsize, d = p)
+		
+		sig_matrix = self.matrix_signature(data, self.last_ensemble)
+		self.last_bins = self.bin_matrix_signature(sig_matrix)
+		self.last_data_id = id(data)
+		
+
+##############################################################################
+################ Tests in need of migration to tests.py ######################
+##############################################################################
 
 def dense_test():
 	dimensions = 1000
@@ -111,36 +145,9 @@ def dense_test():
 	for v, k in bins:
 		print "%-5s %s" % (v,k)
 
-def sparse_test():
-	f = open("otoos11.txt", "r")
-	every_line = []
-	for line in f:
-		line = line.strip().lower()
-		every_line.append(line)
-	every_line = ''.join(every_line)
-	every_line = every_line.split('.')
-	n = len(every_line)
-	dictionary = set()
-	for l in every_line:
-		words = l.split()
-		dictionary.update(set(words))
-	d = len(dictionary)
-	dictionary = dict((word, i) for i, word in enumerate(list(dictionary)))
-	data = dok_matrix((n,d), dtype=np.int32)
-	for j, l in enumerate(every_line):
-		words = l.split()
-		for word in words:
-			data[j, dictionary[word]] = 1
-	data = csr_matrix(data)
-	
-	###################################################################
-	# find average Jaccard distance between completely random points. #
-	###################################################################
-	
 
 if __name__ == "__main__":
 	import numpy as np
 	from time import time
 	#dense_test()
 	sparse_test()
-	
