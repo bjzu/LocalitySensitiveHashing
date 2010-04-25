059fb4e7db02bcacc3a3eb64bb7e96c08e0f608c
Hamilton <hamilton.ulmer@gmail.com>
2010-04-25 11:35:41 -0700

    fixed the current numeric test according to work done on the lsh_nn.py updates.

--- numeric_tests.py
+++ numeric_tests.py
@@ -3,14 +3,15 @@ import lsh_nn as lsh
 from text_tools import txt_to_csr
 
 def sparse_minhash_test():
-    nn = lsh.LSH(hash_function = lsh.make_sparse_minhash)
-    # grab some data.
-    data = txt_to_csr("dd.txt")
-    print "Loaded data (%s by %s) ..." % data.shape
-    nn.bin_data(data, bands = 20, per_band = 5)
-    print "Binned data ..."
-    nearest = nn.return_nearest_neighbors(data, 1)
-    print nearest
+	nn = lsh.LSH(hash_function = lsh.make_sparse_minhash)
+	# grab some data.
+	data = txt_to_csr("otoos11.txt")
+	print "Loaded data (%s by %s) ..." % data.shape
+	nn.bin_data(data, bands = 60, per_band = 5)
+	print "Binned data ..."
+	from pprint import PrettyPrinter
+	p = PrettyPrinter(indent=4)
+	p.pprint(nn.last_bins)
 
 if __name__ == "__main__":
     sparse_minhash_test()
