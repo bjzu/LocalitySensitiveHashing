b5090e476101cd5461d25082ea28937012aa28f6
Hamilton <hamilton@hamilton-desktop.(none)>
2010-04-24 20:11:53 -0700

    started a few tests for distance functions.  Found out that len(set([2]) and set([0])) is one, and len(set([2]) & set([0])) is zero.

--- tests.py
+++ tests.py
@@ -55,6 +55,37 @@ class TestCoreFunctionality(unittest.TestCase):
 		"""LSH should 'work' with make_coordinate_hash"""
 		pass
 		
+class TestDistanceFunctions(unittest.TestCase):
+	def setUp(self):
+		from scipy.sparse import csr_matrix
+		import numpy as np
+		from distance import sparse_jaccard
+
+	def testSparseJaccard(self):
+		"""sparse_jaccard between should work with disjoint, 
+		identical, and partially overlapping vectors."""
+		from scipy.sparse import csr_matrix
+		import numpy as np
+		from distance import sparse_jaccard
+		a = csr_matrix((1,3))
+		a[0,2] = 1
+		b = csr_matrix((1,3))
+		b[0,1] = 1
+		b[0,2] = 1
+		c = csr_matrix((1,3))
+		c[0,0] = 1
+		c[0,1] = 1
+		d = csr_matrix((1,3))
+		d[0,0] = 1
+		sp1 = sparse_jaccard(a,d)
+		sp2 = sparse_jaccard(b,c)
+		sp3 = sparse_jaccard(a,a)
+		sp1 = sp1 == 1
+		sp2 = round(sp2,2)== .67
+		sp3 = sp3 == 0
+		
+		self.assertTrue(sp1 and sp2 and sp3)
+
 
 if __name__ == "__main__":
 	unittest.main()
