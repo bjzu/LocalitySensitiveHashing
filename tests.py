070b8776fc5a96dcc3193a92103caae769f1e375
Hamilton <hamilton.ulmer@gmail.com>
2010-05-27 21:13:44 -0700

    Finished a few of the TestNewObject functions.  As of this commit everything passes.

--- tests.py
+++ tests.py
@@ -21,33 +21,42 @@ class TestNewObject(unittest.TestCase):
 			lsh.LSH(bands=-10)
 		except:
 			self.assert_(True)
-		self.fail("bands was set to -10, should have failed.")
-		
+		else:
+			self.fail("bands was set to -10, should have failed.")
 	
 	def test_too_few_per_band(self):
 		try:
 			lsh.LSH(per_band=-10)
 		except:
 			self.assert_(True)
-		self.fail("per_band was set to -10, should have failed.")
+		else:
+			self.fail("per_band was set to -10, should have failed.")
 	
 	def test_noninteger_bands(self):
 		try:
 			lsh.LSH(bands="foo")
 		except:
 			self.assert_(True)
-		self.fail("tried to set bands to a noninteger value.")
+		else:
+			self.fail("tried to set bands to a noninteger value.")
 	
 	def test_noninteger_per_band(self):
 		try:
 			lsh.LSH(per_band="foo")
 		except:
 			self.assert_(True)
-		self.fail("tried to set per_band to a noninteger value.")
+		else:
+			self.fail("tried to set per_band to a noninteger value.")
 
 class TestWithCachedData(unittest.TestCase):
+	"""
+	
+	
+	"""
 	pass
 
+class TestWithoutCachedData(unittest.TestCase):
+	pass
 
 
 if __name__ == "__main__":
